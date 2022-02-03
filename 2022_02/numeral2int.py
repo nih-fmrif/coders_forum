7ffff1448fe79c963e6fafb0c32bfc8579d3fd90
Eric Earl <eric.earl@nih.gov>
2022-02-03 08:28:12 -0800

    Update numeral2int function
    Added in actual code for testing

--- 2022_02/numeral2int.py
+++ 2022_02/numeral2int.py
@@ -1,4 +1,41 @@
-"""Convert a Roman Numeral to an int"""
+def numeral2int(numeral):
+    """Convert a Roman Numeral to an int"""
 
-def numeral2int(s):
-    pass
+    # forcing all characters to uppercase
+    numeral = numeral.upper()
+
+    # creating a dictionary of roman numerals and their values
+    numeral_dict = {
+                    'I': 1,
+                    'V': 5,
+                    'X': 10,
+                    'L': 50,
+                    'C': 100,
+                    'D': 500,
+                    'M': 1000
+                    }
+
+    # creating a list of the numerals in the string
+    numeral_list = list(numeral)
+
+    for character in numeral_list:
+        if character not in numeral_dict.keys():
+            raise ValueError('Invalid Roman Numeral "' + str(character) + '": using a numeral not contained in the dictionary')
+
+    # reverse the list order
+    numeral_list.reverse()
+
+    # initialize the summator
+    numeral_int = 0
+
+    # iterate through the list
+    for i in range(len(numeral_list)):
+        # if we're at the first element, add the value to the summator
+        if i == 0:
+            # add the first reversed list value
+            numeral_int += numeral_dict[numeral_list[i]]
+        elif numeral_dict[numeral_list[i]] > numeral_dict[numeral_list[i-1]]:
+            numeral_int += numeral_dict[numeral_list[i]]
+        else:
+            numeral_int -= numeral_dict[numeral_list[i]]
+    return numeral_int
