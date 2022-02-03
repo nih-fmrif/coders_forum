2188a558c63754b853112253d8fa6a79ad1644f4
shotgunosine <adenosine@gmail.com>
2022-02-03 11:13:58 -0500

    add a numpy style docstring

--- 2022_02/numeral2int.py
+++ 2022_02/numeral2int.py
@@ -1,4 +1,18 @@
 """Convert a Roman Numeral to an int"""
 
-def numeral2int(s):
+def numeral2int(numeral):
+    """
+    Convert a roman numeral as a string to an integer.
+
+    Parameters
+    ----------
+    s : str
+        Roman numeral to be converted to an integer
+
+
+    Returns
+    -------
+    int
+        Integer implementation 
+    """
     pass
